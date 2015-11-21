a6c77106ec1efcbd5e73cef1de53e5a2a47f5de0
Zhichen Xu <xzc731@hotmail.com>
2015-11-22 10:18:56 +1100

    Revert "Assignment2"
    This reverts commit 730786acd9d25d038fa59a1a8bea2df57b30d4ef.

--- cachematrix.R
+++ cachematrix.R
@@ -21,6 +21,26 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Version 0.1
 ## To retrieve the inverse from the cache if alraedy inversed
 
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+}
+
+
+## This function computes the inverse of the special "matrix" created by 
+## makeCacheMatrix above. If the inverse has already been calculated (and the 
+## matrix has not changed), then it should retrieve the inverse from the cache.
+
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   inv <- x$getInverse()
